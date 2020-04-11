d5be662f98d0857eae4442abea329c3ecf43d4c7
Juan Rodriguez <syn231000@hotmail.com>
2020-04-11 02:42:14 +0200

    git ignore

--- Nivel_Medio/www/mid_6-en-casa.js
+++ Nivel_Medio/www/mid_6-en-casa.js
@@ -6,5 +6,35 @@
  * 
  */
 
-numbers = [1,2,4,5,6,7,8,9,13,14];
+"use strict";
 
+let numbers = [1,2,4,5,6,7,8,9,13,14];
+
+let min = numbers[0];
+let max = numbers[0];
+
+let i
+
+for (i=0; i<numbers.length; i++) {
+    if (i < min ) {
+        min = i;
+    }
+    if (i > max) {
+        max = i
+    }
+}
+console.log(min, max);
+
+
+
+
+/** 
+let number
+
+for (number of numbers) {
+    if (numbers < numbers+1 && numbers<numbers-1) {
+        output.push(number);
+    }
+}
+console.log(output);
+*/
