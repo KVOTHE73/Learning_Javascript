f30b858ef008033ed7a9bf36df34c06902bff986
Juan Rodriguez <syn231000@hotmail.com>
2020-04-20 17:34:55 +0200

    ejercicio 16

--- Nivel_Medio/www/mid_16-const_y_let.js
+++ Nivel_Medio/www/mid_16-const_y_let.js
@@ -17,7 +17,7 @@
  */
 
 
- /*
+ 
  // DESCOMENTA ESTE BLOQUE PARA EJECUTARLO
  // nota el error que da debido a que `passed`
  // no existe fuera del ámbito del `if`
@@ -28,32 +28,34 @@
     const passed = true;
  }
 
- console.log(passed);
-*/
+ // console.log(passed); ==> nos dice que passed no esta definido porque solo existe dentro del if, habria que declararlo fuera: let passed = [];
 
 
-/*
+
+
  // DESCOMENTA ESTE BLOQUE PARA EJECUTARLO
  // nota el error debido a la declaración de dos
  // variables con el mismo nombre
-let miVariable = 0;
-let miVariable = 2;
-*/
+// let miVariable = 0;
+// let miVariable = 2; ==> dice Identifier 'miVariable' has already been declared porque no puede haber dos variables con el mismo nombre en el mismo scope (=nivel)
 
-/*
+
+
  // DESCOMENTA ESTE BLOQUE PARA EJECUTARLO
  // nota cómo la asinación dentro del if se refiere
  // a la variable declarada fuera
-let mensaje = '';
-let nota = 8;
+let mensajes = '';
+let notas = 8;
 
-if (nota>5) {
-   mensaje = 'aprobado';
+if (notas>5) {
+   mensajes = 'aprobado';
+   
+   
 }
 
-console.log(mensaje);
+// console.log(mensaje); ==> Identifier 'mensaje' has already been declared, igual que en el anterior, mensaje esta declarado dos veces.
 
-*/
+
 
 
 
@@ -63,11 +65,26 @@ console.log(mensaje);
  // y fuera del mismo tiene el valor inicial, esto es,
  // cadena vacía
 let mensaje = '';
-let nota = 8;
+let Nota = 8;
 
-if (nota>5) {
+if (Nota>5) {
    let mensaje = 'aprobado';
 }
 
-console.log(mensaje);
+//console.log(mensaje); ==> si ponemos el raton sobre el "mensaje" de dentro del if, que sale en color gris, nos dice que esta declarado pero que nunca sera leido, ya que prevalece el ,que esta declarado afuera, que es una cadena vacia
 
+let nota = 4;
+
+if (nota>5) {
+   let calificacion = 'aprobado';
+   console.log(calificacion);
+   
+}
+if (nota<5) {
+   let calificacion = 'suspenso';
+   console.log(calificacion);
+   
+} 
+
+// Aqui tenemos dos variables con el mismo nombre pero al existir solo dentro de sus respectivos if si funciona sin problema.
+// console.log(calificacion);==> pero tendremos que hacer los log dentro de cada if, si lo ponemos afuera nos dira calificacion is not defined porque calificacion no existe fuera del if
