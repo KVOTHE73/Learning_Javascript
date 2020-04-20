f7cd5ab4e5ebfd40c17f0c0f3af1464445316b26
Juan Rodriguez <syn231000@hotmail.com>
2020-04-20 03:47:57 +0200

    ejercicio 15b

--- Nivel_Medio/www/mid_15b-Funcion_Comparacion_Dos_Arrays.js
+++ Nivel_Medio/www/mid_15b-Funcion_Comparacion_Dos_Arrays.js
@@ -20,11 +20,48 @@
  * 
  *    El array resultante será
  *              [true, true, false, false] 
- * 
+ *****MODIFICACION AL EJERCICIO ORIGINAL, CAMBIAMOS TRUE POR "MULTA" Y FALSE POR "NO MULTA"*****
+ *  
  *    La penalización será: ((90-80) + (130-120))*2/10 = 4
+ * 
+ *  USAMOS FUNCIONES PARA RESOLVER EL EJERCICIO
  * 
  */
 
+"use strict";
+
 const speeds = [100, 105, 90, 130];
 const maxSpeeds = [120, 120, 80, 120];
+let i = 0;
+
+function multaOnoMulta(speeds, maxSpeeds) {
+    if (speeds <= maxSpeeds) {
+        return speeds = "No Multa" 
+        }    else {
+        return speeds = "Multa";
+    }
+}
+
+function calculatePenalizacion(speeds, maxSpeeds) {
+    if (speeds > maxSpeeds) {
+        return ((speeds - maxSpeeds) * 2) / 10;
+        } else {
+            return 0;
+        }
+}
+
+let multa = [];
+let penalizacion = 0;
+let baremo = [];
+
+for (let i=0; i<speeds.length; i++) {
+    baremo = multaOnoMulta(speeds[i], maxSpeeds[i]);
+    multa.push(baremo);
+    penalizacion += calculatePenalizacion(speeds[i], maxSpeeds[i]);
+}
+
+console.log(`Multas por tramo: ${multa}`);
+
+console.log(`La penalización será de ${penalizacion} puntos`);
+
 
